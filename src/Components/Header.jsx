97bc1a9d6669440276b787a7acec16c5286bcb29
AbdulMoiz8994 <moiza8994@gmail.com>
2021-04-09 19:40:06 +0500

    we are making expense track app
    we have made components,and types files and import in componets in single file of index.js

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div>
+            <h1>Header Par</h1>
+        </div>
+    )
+}
---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+export { Header } from "./Header";
+export { History } from "./History";
+export { Balance } from "./Balance";
+export { AddTransaction } from "./AddTransaction";
+export { IncomeAndExpense } from "./IncomeAndExpense";
